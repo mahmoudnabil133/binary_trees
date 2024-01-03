0bbda2882b1dc3a8f578d4ab96b4238b80c6d59c
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-03 23:45:51 +0200

    del

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,13 +12,17 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (!tree)
 		return;
 	if (!tree->left && !tree->right)
+	{
 		free(tree);
 		return;
+	}
 	if (tree->left)
 		binary_tree_delete(tree->left);
 	if (tree->right)
 		binary_tree_delete(tree->right);
 	if (tree)
+	{
 		free(tree);
 		return;
+	}
 }

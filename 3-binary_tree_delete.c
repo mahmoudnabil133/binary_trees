65408902dc971678b0f9fe52b9dab715fb1f33be
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-03 23:37:40 +0200

    del

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete a BT
+ * @tree: ptr to root
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if (!tree->left && !tree->right)
+		free(tree);
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree)
+		free(tree);
+}

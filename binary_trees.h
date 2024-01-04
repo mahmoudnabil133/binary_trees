0f18c29a8f2343a8e5f64f23817343e9bc7fe80f
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-05 00:54:31 +0200

    binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,5 @@ int max(int x, int y);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */

f422569f561a9f3ca273efd5f887ac621ce3d367
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-04 18:12:41 +0200

    root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (!node->left || !node->right || !node->parent)
-		return (0);
-	return (1);
+	if (!node->parent)
+        return (1);
+	return (0);
 
 }

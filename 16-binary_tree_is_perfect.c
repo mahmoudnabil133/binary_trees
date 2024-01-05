52bb9120be8d426b46bd6909e313277ca84775b8
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-05 02:13:28 +0200

    binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,51 +1,16 @@
 #include "binary_trees.h"
 #include "binary_trees.h"
 /**
- * max - max of 2 int
- * @x: x
- * @y: y
- * Return: int
-*/
-int max(int x, int y)
-{
-	if (x >= y)
-		return (x);
-	return (y);
-}
-/**
- * binary_tree_is_root - check if the node is leaf or not
- * @node: node to check
- * Return: int
-*/
-int binary_tree_is_root(const binary_tree_t *node)
-{
-	if (!node)
-		return (0);
-	if (!node->parent)
-		return (1);
-	return (0);
-
-}
-/**
- * binary_tree_height - height of tree
- * @tree: ptr to head
- * Return:height
+ * binary_tree_size - return size
+ * @tree: ptr to tree root
+ * Return: size of the tree
  *
 */
-size_t binary_tree_height(const binary_tree_t *tree)
-{size_t left_height = 0, right_height = 0;
-
+size_t binary_tree_size(const binary_tree_t *tree)
+{
 	if (!tree)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (binary_tree_is_root(tree))
-	{
-		left_height -= 1;
-		right_height -= 1;
-	}
-	return (max(left_height, right_height) + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
 /**
@@ -71,7 +36,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
 		if (binary_tree_is_full(tree))
 			return (1);
 	return (0);

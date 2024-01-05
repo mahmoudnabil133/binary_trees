5081e22549027fdd7b31c24221b193169ed76f2d
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-05 17:07:28 +0200

    binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,31 +1,5 @@
 #include "binary_trees.h"
 /**
- * max - max of 2 int
- * @x: x
- * @y: y
- * Return: int
-*/
-int max(int x, int y)
-{
-	if (x >= y)
-		return (x);
-	return (y);
-}
-/**
- * binary_tree_is_root - check if the node is leaf or not
- * @node: node to check
- * Return: int
-*/
-int binary_tree_is_root(const binary_tree_t *node)
-{
-	if (!node)
-		return (0);
-	if (!node->parent)
-		return (1);
-	return (0);
-
-}
-/**
  * binary_tree_height - height of tree
  * @tree: ptr to head
  * Return:height
@@ -34,15 +8,13 @@ int binary_tree_is_root(const binary_tree_t *node)
 size_t binary_tree_height(const binary_tree_t *tree)
 {size_t left_height = 0, right_height = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	if (binary_tree_is_root(tree))
-	{
-		left_height -= 1;
-		right_height -= 1;
-	}
-	return (max(left_height, right_height) + 1);
+
+	if (left_height >= right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }

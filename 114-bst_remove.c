0d493aed31cb35c9eac49f4d3caed8698ab20a1c
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-07 16:09:10 +0200

    binary tree

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -7,25 +7,27 @@
 int getMin(bst_t *root)
 {int min;
 
-    if (!root->left)
-    {
-        root->parent->right = root->right;
-        if (root->right)
-        {
-            root->right->parent = root->parent;
-        }
-        min = root->n;
-        free(root);
-        return(min);
-    }
-    while(root->left)
-    {
-        root = root->left;
-    }
-    min = root->n;
-    root->parent->left = NULL;
-    free(root);
-    return (min);
+	if (!root->left)
+	{
+		root->parent->right = root->right;
+		if (root->right)
+		{
+			root->right->parent = root->parent;
+		}
+		min = root->n;
+		free(root);
+		return (min);
+	}
+	while (root->left)
+	{
+		root = root->left;
+	}
+	min = root->n;
+	root->parent->left = root->right;
+	if (root->right)
+		root->right->parent = root->parent;
+	free(root);
+	return (min);
 }
 /**
  * bst_remove - rm node from bst.
@@ -35,39 +37,32 @@ int getMin(bst_t *root)
 */
 bst_t *bst_remove(bst_t *root, int value)
 {bst_t *newLink;
-    if (!root)
-        return (root);
-    if (value < root->n)
-    {
-        root->left = bst_remove(root->left, value);
-    }
-    else if (value > root->n)
-    {
-        root->right = bst_remove(root->right, value);
-    }
-    else
-    {
-        if (!root->right)
-        {
-            newLink = root->left;
-            if (newLink)
-                newLink->parent = root->parent;
-            free(root);
-            return (newLink);
-        }
-        else if (!root->left)
-        {
-            newLink = root->right;
-            if (newLink)
-                newLink->parent = root->parent;
-            free(root);
-            return (newLink);
-        }
-        else
-        {
-            root->n = getMin(root->right);
-            return (root);
-        }
-    }
-    return (root);
+	if (!root)
+		return (root);
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->right)
+		{
+			newLink = root->left;
+			if (newLink)
+				newLink->parent = root->parent;
+			free(root);
+			return (newLink);
+		}
+		else if (!root->left)
+		{
+			newLink = root->right;
+			if (newLink)
+				newLink->parent = root->parent;
+			free(root);
+			return (newLink);
+		}
+		else
+			root->n = getMin(root->right);
+	}
+	return (root);
 }
